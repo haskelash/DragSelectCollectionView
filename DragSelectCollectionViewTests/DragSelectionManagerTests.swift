ac25de4a53ad9e72f520d255bb24008c86d0e05f
Haskel Ash <haskelash@gmail.com>
2016-12-04 13:28:48 -0800

    add unit tests file for selection manager

---
+++ DragSelectCollectionViewTests/DragSelectionManagerTests.swift
@@ -0,0 +1,25 @@
+//
+//  DragSelectionManager.swift
+//  DragSelectCollectionView
+//
+//  Created by Haskel Ash on 12/4/16.
+//  Copyright © 2016 HaskelAsh. All rights reserved.
+//
+
+import XCTest
+@testable import DragSelectCollectionView
+
+class DragSelectionManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+
+    }
+}
