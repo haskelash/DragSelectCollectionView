f85fb376f20b6c5bc0f7455e31ffc925ff51e08f
Haskel Ash <haskelash@gmail.com>
2018-03-14 11:07:24 -0700

    remove old tests, add tests for max selection count, markers for other tests

--- DragSelectCollectionViewTests/DragSelectionManagerTests.swift
+++ DragSelectCollectionViewTests/DragSelectionManagerTests.swift
@@ -11,70 +11,152 @@ import XCTest
 
 class DragSelectionManagerTests: XCTestCase {
 
+    var selectionManager: DragSelectionManager!
     var collectionView: UICollectionView!
-    var dataSource: MockDataSource!
-    static var exp: XCTestExpectation!
+    var mockDataSource: MockDataSource!
+
+    //MARK: SET UP / TEAR DOWN
 
     override func setUp() {
         super.setUp()
-        collectionView = UICollectionView(frame: CGRect.zero,
-                                          collectionViewLayout: UICollectionViewLayout())
-        dataSource = MockDataSource()
-        collectionView.dataSource = dataSource
+        collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        mockDataSource = MockDataSource()
+        collectionView.dataSource = mockDataSource
+        selectionManager = DragSelectionManager(collectionView: collectionView)
+        collectionView.allowsMultipleSelection = true
     }
     
     override func tearDown() {
+        selectionManager = nil
+        collectionView = nil
+        mockDataSource = nil
         super.tearDown()
     }
-    
-    func testSetSelected() {
-        //given a selection manager and an index path
-        let selectionManager = SelectionManagerExpectingCalls(collectionView: collectionView)
-        collectionView.delegate = selectionManager
-        let path = IndexPath(item: 3, section: 2)
-        type(of: self).exp = expectation(description: "Method didSelectItemAtIndexPath: should be called.")
 
-        //when the selection manager selects that path
-        selectionManager.setSelected(true, for: path)
+    //MARK: SETTING MAX SELECTION
 
-        //then selectedIndicies should contain that path and the delegate method should be called
-        XCTAssert(selectionManager.selectedIndices.contains(path))
-        waitForExpectations(timeout: 1, handler: nil)
+
+    func testSettingMaxSelectionCountLimitsSelection() {
+        //given a selection manager with no selection limit
+        selectionManager.maxSelectionCount = 0
+
+        //when I set a selection limit of 5 and try to select a 6th item
+        selectionManager.maxSelectionCount = 5
+        (0..<6).forEach() {
+            selectionManager.setSelected(true, for: IndexPath(item: $0, section: 0))
+        }
+
+        //then the first 5 items should be selected and the 6th item should not be selected
+        guard let selections = collectionView.indexPathsForSelectedItems else {
+            XCTFail("Selected indices is empty.")
+            return
+        }
+
+        (0..<5).forEach() {
+            XCTAssert(selections.contains(IndexPath(item: $0, section: 0)),
+                      "Selected indices does not include [\(0), \($0)].")
+        }
+        XCTAssertFalse(selections.contains(IndexPath(item: 5, section: 0)),
+                       "Selected indices does include [\(0), \(5)] when it should not.")
     }
 
-    func testSetSelectedAgain() {
-        //given a selection manager and an index path that has already been selected
-        let selectionManager = SelectionManagerExpectingCalls(collectionView: collectionView)
-        collectionView.delegate = nil
-        let path = IndexPath(item: 3, section: 2)
-        selectionManager.setSelected(true, for: path)
+    func testSettingMaxSelectionCountWhenTooManyAreSelected() {
+        //given a selection manager with no selection limit and 9 selected items
+        selectionManager.maxSelectionCount = nil
+        (0..<9).forEach() {
+            selectionManager.setSelected(true, for: IndexPath(item: $0, section: 0))
+        }
 
-        //when the selection manager selects that path again
-        collectionView.delegate = SelectionManagerNotExpectingCalls(collectionView: collectionView)
-        selectionManager.setSelected(true, for: path)
+        //when I set a selection limit of 5
+        selectionManager.maxSelectionCount = 5
 
-        //then selectedIndicies should contain that path and the delegate method should not be called
-        XCTAssert(selectionManager.selectedIndices.contains(path))
+        //then the first 5 items should be selected and the last 4 items shoud be deselected
+        guard let selections = collectionView.indexPathsForSelectedItems else {
+            XCTFail("Selected indices is empty.")
+            return
+        }
+
+        (0..<5).forEach() {
+            XCTAssert(selections.contains(IndexPath(item: $0, section: 0)),
+                      "Selected indices does not include [\(0), \($0)].")
+        }
+
+        (5..<9).forEach() {
+            XCTAssertFalse(selections.contains(IndexPath(item: $0, section: 0)),
+                           "Selected indices does include [\(0), \($0)] when it should not.")
+        }
     }
-}
 
-class SelectionManagerExpectingCalls: DragSelectionManager {
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        DragSelectionManagerTests.exp.fulfill()
-    }
-}
+    func testDisablingMaxSelection() {
+        //given a selection manager with a selection limit of 5
+        selectionManager.maxSelectionCount = 5
 
-class SelectionManagerNotExpectingCalls: DragSelectionManager {
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        XCTFail("Method didSelectItemAtIndexPath: was called when it shouldn't be.")
+        //when I remove the selection limit and select 6 items
+        selectionManager.maxSelectionCount = nil
+        (0..<6).forEach() {
+            selectionManager.setSelected(true, for: IndexPath(item: $0, section: 0))
+        }
+
+        //then all 6 items should be selected
+        guard let selections = collectionView.indexPathsForSelectedItems else {
+            XCTFail("Selected indices is empty.")
+            return
+        }
+
+        (0..<6).forEach() {
+            XCTAssert(selections.contains(IndexPath(item: $0, section: 0)),
+                      "Selected indices does not include [\(0), \($0)].")
+        }
     }
+
+    func testSettingMaxSelectionOfZero() {
+        //given a selection manager with no selection limit
+        selectionManager.maxSelectionCount = nil
+
+        //when I set a selection limit of 0 and try to select an item
+        selectionManager.maxSelectionCount = 0
+        selectionManager.setSelected(true, for: IndexPath(item: 0, section: 0))
+
+        //then it should not be selected
+        guard let selections = collectionView.indexPathsForSelectedItems else {
+            XCTFail("Selected indices is empty.")
+            return
+        }
+
+        XCTAssertFalse(selections.contains(IndexPath(item: 0, section: 0)),
+                       "Selected indices does include [\(0), \(0)] when it should not.")
+    }
+
+    func testSettingNegativeMaxSelection() {
+        //given a selection manager with no selection limit
+        selectionManager.maxSelectionCount = nil
+
+        //when I set a selection limit of -1 and try to select an item
+        selectionManager.maxSelectionCount = -1
+        selectionManager.setSelected(true, for: IndexPath(item: 0, section: 0))
+
+        //then it should not be selected
+        guard let selections = collectionView.indexPathsForSelectedItems else {
+            XCTFail("Selected indices is empty.")
+            return
+        }
+
+        XCTAssertFalse(selections.contains(IndexPath(item: 0, section: 0)),
+                       "Selected indices does include [\(0), \(0)] when it should not.")
+    }
+
+    //MARK: SETTING SELECTED
+
+    //MARK: TOGGLING SELECTION
+
+    //MARK: SELECTING RANGE
+
+    //MARK: SELECTING ALL
+
+    //MARK: DESELECTING ALL
 }
 
 class MockDataSource: NSObject, UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 5
-    }
-
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
@@ -83,3 +165,4 @@ class MockDataSource: NSObject, UICollectionViewDataSource {
         return UICollectionViewCell()
     }
 }
+
