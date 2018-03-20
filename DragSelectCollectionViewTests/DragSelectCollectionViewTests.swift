fa454c3a88a73bb319b4ad4215de72e972bd6cd0
Haskel Ash <haskelash@gmail.com>
2018-03-19 17:37:07 -0700

    wip

--- DragSelectCollectionViewTests/DragSelectCollectionViewTests.swift
+++ DragSelectCollectionViewTests/DragSelectCollectionViewTests.swift
@@ -10,14 +10,52 @@ import XCTest
 @testable import DragSelectCollectionView
 
 class DragSelectCollectionViewTests: XCTestCase {
-    
+
+    var collectionView: DragSelectCollectionView!
+
+    //MARK: SET UP / TEAR DOWN
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        collectionView = DragSelectCollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.allowsMultipleSelection = true
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        collectionView = nil
         super.tearDown()
+    }
+
+    //MARK: STARTING DRAG SELECTION
+
+    func testBeginDragSelection() {
+        //given a selectable index path
+        let path = IndexPath(item: 0, section: 0)
+
+        //when I start selection with the index
+        let began = collectionView.beginDragSelection(at: path)
+
+        //then selection should begin
+        XCTAssertTrue(began, "Drag selection did not start.")
+    }
+
+    //MARK: CONTINUING DRAG SELECTION
+
+    func testContinueDragSelection() {
+        //given a collection view that has started a selection event
+        let path = IndexPath(item: 0, section: 0)
+        collectionView.beginDragSelection(at: path)
+
+        //when I continue that event
+        collectionView.touchesMoved([], with: FakeEvent())
+
+        //then the continued selection should be reflectd in the selected indicies
+        
+    }
+}
+
+class FakeEvent: UIEvent {
+    override var allTouches: Set<UITouch>? {
+        return [UITouch()]
     }
 }
